8eabf97502a062242217bd2daf560c7d73acfb20
יאיר לוי <supraniti.inbox@gmail.com>
2018-05-23 15:45:44 +0300

    test

--- gh-pages/script.js
+++ gh-pages/script.js
@@ -1,17 +1,15 @@
 
 function fetchUI(){
-  let UI = document.createDocumentFragment();
   fetch('/UI-Templates/controllers.html')
   .then(function (response) {
     return response.text();
   })
   .then(function (html) {
-    UI.innerHTML = html;
-    //console.log(text);
+    return document.createRange().createContextualFragment(html);
   });
-  return UI;
+  return false;
 }
 let test = document.getElementById('testUI');
 test.onclick = function(){
-  console.log( fetchUI().children );
+  console.log( fetchUI() );
 }
